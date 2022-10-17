ab277bfe3508e779555127b7345b1926c8b9daea
ZolF <zolf@bk.ru>
2022-10-17 21:09:23 +0300

    resolve_all_reviewers_offers

--- static/pages/chats/chats.js
+++ static/pages/chats/chats.js
@@ -26,36 +26,36 @@ function fillFriends(array) {
 function createFriendElem(friend) {
     const time = ` ${timeNow.getDate()} ${timeNow.getMonth()} ${timeNow.getFullYear()}`;
     const text = friend.company.catchPhrase;
-    const a_url = `https://avatars.dicebear.com/api/human/${friend.username}.svg`;
+    const aUrl = `https://avatars.dicebear.com/api/human/${friend.username}.svg`;
     const username = friend.name;
 
     const message = makeDiv('message', 'div');
     enterPoint.append(message);
 
-    const message__avatar = makeDiv('message__avatar', 'img');
-    message.append(message__avatar);
-    message__avatar.src = a_url;
+    const messageAvatar = makeDiv('message__avatar', 'img');
+    message.append(messageAvatar);
+    messageAvatar.src = aUrl;
 
-    const message__main = makeDiv('message__main', 'div');
-    message.append(message__main);
+    const messageMain = makeDiv('message__main', 'div');
+    message.append(messageMain);
 
-    const message__message = makeDiv('message__message', 'div');
-    message__main.append(message__message);
+    const messageMessage = makeDiv('message__message', 'div');
+    messageMain.append(messageMessage);
 
-    const message__bottom = makeDiv('message__bottom', 'div');
-    message__main.append(message__bottom);
+    const messageBottom = makeDiv('message__bottom', 'div');
+    messageMain.append(messageBottom);
 
-    const message__username = makeDiv('message__username', 'p');
-    message__message.append(message__username);
-    message__username.innerText = username;
+    const messageUsername = makeDiv('message__username', 'p');
+    messageMessage.append(messageUsername);
+    messageUsername.innerText = username;
 
-    const message__time = makeDiv('message__time', 'p');
-    message__message.append(message__time);
-    message__time.innerText = time;
+    const messageTime = makeDiv('message__time', 'p');
+    messageMessage.append(messageTime);
+    messageTime.innerText = time;
 
-    const message__messageText = makeDiv('message__messageText', 'p');
-    message__bottom.append(message__messageText);
-    message__messageText.innerText = text;
+    const messageMessageText = makeDiv('message__messageText', 'p');
+    messageBottom.append(messageMessageText);
+    messageMessageText.innerText = text;
 }
 
 getFriends();
@@ -80,11 +80,3 @@ function hideElement(btn, toHideElem) {
 
 hideElement('.screen__chat-input__add', '.screen__chat__menu');
 hideElement('.screen__chat-button', '.screen__chat__toggleMenu');
-
-// const clinch = document.querySelector('.screen__chat-input__add');
-// clinch.addEventListener('click', () => {
-//     const clinchMenu = document.querySelector('.screen__chat__menu');
-//     clinchMenu.classList.toggle('hide');
-// });
-
-// screen__chat - button;
